1e62e60348fd457d4901aa138661bdc965ff09e7
INGRID <ingridcarolinelima@hotmail.com>
2019-09-29 16:14:21 -0300

    Projeto criado

---
+++ src/main/java/com/ingrid/udemycurso/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.ingrid.udemycurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
