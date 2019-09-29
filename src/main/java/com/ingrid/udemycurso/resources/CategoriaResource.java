537ad603a26ed57317ae6d3d8fd0d5f497fa65ed
INGRID <ingridcarolinelima@hotmail.com>
2019-09-29 16:35:32 -0300

    Testando rest

---
+++ src/main/java/com/ingrid/udemycurso/resources/CategoriaResource.java
@@ -0,0 +1,14 @@
+package com.ingrid.udemycurso.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+}
